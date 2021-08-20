25c0e5c40353a10b46af2f362bae51074aa0924d
Sherin Joannah <88616871+SherinJoannah13@users.noreply.github.com>
2021-08-20 11:48:23 +0530

    Add files via upload

---
+++ MODEL WINDOW(BTS)/script.js
@@ -0,0 +1,123 @@
+'use strict'
+
+const image = document.querySelector('.image');
+const overlay = document.querySelector('.overlay');
+const btnCloseImage = document.querySelector('.close-image');
+const btnsModelImage = document.querySelectorAll('.model-image');
+console.log(btnsModelImage);
+for (let i = 0; i < btnsModelImage.length; i++)
+    btnsModelImage[i].addEventListener('click',function(){
+        console.log("button clicked");
+        image.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+btnCloseImage.addEventListener('click',function(){
+    image.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+const image1 = document.querySelector('.image1');
+const overlay1 = document.querySelector('.overlay1');
+const btnCloseImage1 = document.querySelector('.close-image1');
+const btnsModelImage1 = document.querySelectorAll('.model-image1');
+console.log(btnsModelImage1);
+for (let i = 0; i < btnsModelImage1.length; i++)
+    btnsModelImage1[i].addEventListener('click',function(){
+        console.log("button clicked");
+        image1.classList.remove('hidden');
+        overlay1.classList.remove('hidden');
+    });
+btnCloseImage1.addEventListener('click',function(){
+    image1.classList.add('hidden');
+     overlay1.classList.add('hidden');
+});
+const image2 = document.querySelector('.image2');
+const overlay2 = document.querySelector('.overlay2');
+const btnCloseImage2 = document.querySelector('.close-image2');
+const btnsModelImage2 = document.querySelectorAll('.model-image2');
+console.log(btnsModelImage2);
+for (let i = 0; i < btnsModelImage2.length; i++)
+    btnsModelImage2[i].addEventListener('click',function(){
+        console.log("button clicked");
+       image2.classList.remove('hidden');
+        overlay2.classList.remove('hidden');
+    });
+btnCloseImage2.addEventListener('click',function(){
+    image2.classList.add('hidden');
+    overlay2.classList.add('hidden');
+    });
+const image3 = document.querySelector('.image3');
+const overlay3 = document.querySelector('.overlay3');
+const btnCloseImage3 = document.querySelector('.close-image3');
+const btnsModelImage3 = document.querySelectorAll('.model-image3');
+console.log(btnsModelImage3);
+for (let i = 0; i < btnsModelImage3.length; i++)
+    btnsModelImage3[i].addEventListener('click',function(){
+        console.log("button clicked");
+        image3.classList.remove('hidden');
+        overlay3.classList.remove('hidden');
+    });
+    btnCloseImage3.addEventListener('click',function(){
+        image3.classList.add('hidden');
+         overlay3.classList.add('hidden');
+    });
+const image4 = document.querySelector('.image4');
+const overlay4 = document.querySelector('.overlay4');
+const btnCloseImage4 = document.querySelector('.close-image4');
+const btnsModelImage4 = document.querySelectorAll('.model-image4');
+console.log(btnsModelImage4);
+for (let i = 0; i < btnsModelImage4.length; i++)
+    btnsModelImage4[i].addEventListener('click',function(){
+        console.log("button clicked");
+        image4.classList.remove('hidden');
+        overlay4.classList.remove('hidden');
+    });
+    btnCloseImage4.addEventListener('click',function(){
+        image4.classList.add('hidden');
+         overlay4.classList.add('hidden');
+    });
+const image5 = document.querySelector('.image5');
+const overlay5 = document.querySelector('.overlay5');
+const btnCloseImage5 = document.querySelector('.close-image5');
+ const btnsModelImage5 = document.querySelectorAll('.model-image5');
+console.log(btnsModelImage5);
+for (let i = 0; i < btnsModelImage5.length; i++)
+        btnsModelImage5[i].addEventListener('click',function(){
+            console.log("button clicked");
+            image5.classList.remove('hidden');
+            overlay5.classList.remove('hidden');
+        });
+        btnCloseImage5.addEventListener('click',function(){
+            image5.classList.add('hidden');
+             overlay5.classList.add('hidden');
+        });
+const image6 = document.querySelector('.image6');
+const overlay6 = document.querySelector('.overlay6');
+const btnCloseImage6 = document.querySelector('.close-image6');
+ const btnsModelImage6 = document.querySelectorAll('.model-image6');
+console.log(btnsModelImage6);
+for (let i = 0; i < btnsModelImage6.length; i++)
+        btnsModelImage6[i].addEventListener('click',function(){
+            console.log("button clicked");
+            image6.classList.remove('hidden');
+            overlay6.classList.remove('hidden');
+        });
+        btnCloseImage6.addEventListener('click',function(){
+            image6.classList.add('hidden');
+             overlay6.classList.add('hidden');
+        });
+const image7 = document.querySelector('.image7');
+const overlay7 = document.querySelector('.overlay7');
+const btnCloseImage7 = document.querySelector('.close-image7');
+ const btnsModelImage7 = document.querySelectorAll('.model-image7');
+console.log(btnsModelImage7);
+for (let i = 0; i < btnsModelImage7.length; i++)
+        btnsModelImage7[i].addEventListener('click',function(){
+            console.log("button clicked");
+            image7.classList.remove('hidden');
+            overlay7.classList.remove('hidden');
+        });
+        btnCloseImage7.addEventListener('click',function(){
+            image7.classList.add('hidden');
+             overlay7.classList.add('hidden');
+        });
